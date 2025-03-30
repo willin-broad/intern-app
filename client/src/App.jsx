51b98e8e3cbdde974c55608eb53c6105c890ef2e
1ntr1ns1c <ikipkogey@kabarak.ac.ke>
2025-03-30 10:35:44 +0300

    updated the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,6 @@ import './App.css'
 import Home from "./pages/Home/Home"
 import NotFound from "./pages/NotFound";
 import CheckStatus from "./pages/CheckStatus/CheckStatus";
-import PersonalInfo from "./components/forms/PersonalInfo";
-import EduBackground from "./components/forms/EduBackground";
-import WorkExperience from "./components/forms/WorkExperience";
-import Uploads from "./components/forms/Uploads";
 import Apply from './pages/Apply/Apply';
 
 function App() {
@@ -18,10 +14,6 @@ function App() {
         <Route path="/apply" element={<Apply />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/CheckStatus" element={<CheckStatus />} />
-        <Route path="/PersonalInfo" element={<PersonalInfo />} />
-        <Route path="/EduBackground" element={<EduBackground />} />
-        <Route path="/WorkExperience" element={<WorkExperience />} />
-        <Route path="/Uploads" element={<Uploads />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
