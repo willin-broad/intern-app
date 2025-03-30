418da1e4aef099a777ffe9b449ca84825c418e03
1ntr1ns1c <ikipkogey@kabarak.ac.ke>
2025-03-30 10:04:56 +0300

    updated the routes and added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,31 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from "./pages/Home/Home"
 import NotFound from "./pages/NotFound";
 import CheckStatus from "./pages/CheckStatus/CheckStatus";
-
+import PersonalInfo from "./components/forms/PersonalInfo";
+import EduBackground from "./components/forms/EduBackground";
+import WorkExperience from "./components/forms/WorkExperience";
+import Uploads from "./components/forms/Uploads";
+import Apply from './pages/Apply/Apply';
 
 function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-      errorElement: <NotFound />
-    },
-    {
-      path: "/Home",
-      element: <Home />,
-    },
-    {
-      path: '/CheckStatus',
-      element: <CheckStatus />
-    },
-  ]);
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply" element={<Apply />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/CheckStatus" element={<CheckStatus />} />
+        <Route path="/PersonalInfo" element={<PersonalInfo />} />
+        <Route path="/EduBackground" element={<EduBackground />} />
+        <Route path="/WorkExperience" element={<WorkExperience />} />
+        <Route path="/Uploads" element={<Uploads />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
