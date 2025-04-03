cb289337b818af48f65f9a5fb730fa4a20b3033f
willin-broad <broadwillin@gmail.com>
2025-04-03 12:13:07 +0300

    updated models

--- server/intern_app/settings.py
+++ server/intern_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',#for rest api
+    'rest_framework.authtoken',#for token authentication
     'corsheaders', #allow coomunication with frontend
     'internships',#app for internship
 ]
