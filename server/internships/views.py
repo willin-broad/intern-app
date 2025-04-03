29b2be499ff78a4c5afce334ab191797488f1a09
1ntr1ns1c <ikipkogey@kabarak.ac.ke>
2025-04-03 21:52:35 +0300

    added more viewsets

--- server/internships/views.py
+++ server/internships/views.py
@@ -1,6 +1,15 @@
-from rest_framework import viewsets
-from .models import Intern, Department
-from .serializers import InternSerializer, DepartmentSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import (
+    Intern, Department, EducationBackground, 
+    WorkExperience, UploadedFiles
+)
+from .serializers import (
+    InternSerializer, DepartmentSerializer, 
+    EducationSerializer, WorkExperienceSerializer,
+    UserDepartmentSerializer, UploadSerializer
+)
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class DepartmentViewSet(viewsets.ModelViewSet):
@@ -12,3 +21,64 @@ class InternViewSet(viewsets.ModelViewSet):
     queryset = Intern.objects.all()
     serializer_class = InternSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        intern = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_201_CREATED, 
+            headers=headers
+        )
+
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = EducationBackground.objects.all()
+    serializer_class = EducationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        education = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_201_CREATED, 
+            headers=headers
+        )
+
+class WorkExperienceViewSet(viewsets.ModelViewSet):
+    queryset = WorkExperience.objects.all()
+    serializer_class = WorkExperienceSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        work_exp = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_201_CREATED, 
+            headers=headers
+        )
+
+class DepartmentPreferenceViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = UserDepartmentSerializer
+    basename = 'department-preferences'  # Add unique basename
+
+class UploadedFilesViewSet(viewsets.ModelViewSet):
+    queryset = UploadedFiles.objects.all()
+    serializer_class = UploadSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        files = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_201_CREATED, 
+            headers=headers
+        )
